8d94f26f70918c4c13c5ef23f4d3fa29e9ed7dda
SHITOL-CSE-RU <shitol.cse@gmail.com>
2022-04-12 02:47:25 +0600

    social icon add

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {FaFacebookSquare} from "react-icons/fa"
+import {BsInstagram} from "react-icons/bs"
+import {BsLinkedin} from "react-icons/bs"
+import {BsGithub} from "react-icons/bs"
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+    <a href="https://www.facebook.com/shitol.cse" target="_blank"><FaFacebookSquare /></a>
+    <a href="https://www.instagram.com/shitol.cse" target="_blank"><BsInstagram /></a>
+    <a href="https://www.linkedin.com/shitol.cse" target="_blank"><BsLinkedin /></a>
+    <a href="https://www.github.com/SHITOL-CSE-RU" target="_blank"><BsGithub /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
