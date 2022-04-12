2a7d8c8c0fe93202d7032b6fcbc7ac5bd318be8c
SHITOL-CSE-RU <shitol.cse@gmail.com>
2022-04-13 02:30:11 +0600

    About Section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
+import ME from "../../assets/me-about.jpg";
+import {FaAward} from "react-icons/fa"
+import {FiUsers} from "react-icons/fi"
+import {VscFolderLibrary} from "react-icons/vsc"
 
 const About = () => {
   return (
-    <section id="about">About</section>
-  )
-}
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>30+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Project</h5>
+              <small>15+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+          I am young, energetic, dynamic, an outgoing pleasant personality and self-starter, 
+          strong inter-personal skill with an ability to work at a high pace on one’s own imitative. 
+          As a software developer, I have robust problem-solving skills and proven experience 
+          in creating and designing software in a test-driven environment.
+          </p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default About;
