9407cd316c548f74b5ed5956f61d843e1bbf824d
SHITOL-CSE-RU <shitol.cse@gmail.com>
2022-04-13 12:53:24 +0600

    Experience Section Continue

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -13,33 +13,45 @@ export const Experience = () => {
           <div className="experience__content">
             <article className="experience__details">
               <BsPatchCheckFill />
+              <div>
               <h4>HTML</h4>
               <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
+              <div>
               <h4>CSS</h4>
               <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
+              <div>
               <h4>Bootstrap</h4>
               <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
+              <div>
               <h4>JavaScript</h4>
               <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
+              <div>
               <h4>React</h4>
               <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
+              <div>
               <h4>Taliwind</h4>
               <small className="text-light">Experienced</small>
+              </div>
             </article>
           </div>
         </div>
@@ -47,38 +59,87 @@ export const Experience = () => {
         {/* ================ END OF FRONTEND ================ */}
 
         <div className="experience__backend">
-        <div className="experience__frontend">
           <h3>Backend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
               <BsPatchCheckFill />
+              <div>
               <h4>Node JS</h4>
               <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
+              <div>
               <h4>MongoDB</h4>
               <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
+              <div>
               <h4>PHP</h4>
               <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>MySQL</h4>
+              <div><h4>MySQL</h4>
               <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
+              <div>
               <h4>Python</h4>
               <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+ {/* ================ END OF BACKEND ================ */}
+
+        <div className="experience__Language">
+          <h3>Programming Language</h3>
+          <div className="experience__content">
+          <article className="experience__details">
+              <BsPatchCheckFill />
+              <div>
+              <h4>Node JS</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <div>
+              <h4>MongoDB</h4>
+              <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <div>
+              <h4>PHP</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <div><h4>MySQL</h4>
+              <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <div>
+              <h4>Python</h4>
+              <small className="text-light">Intermediate</small>
+              </div>
             </article>
           </div>
         </div>
         </div>
-      </div>
     </section>
   )
 }
