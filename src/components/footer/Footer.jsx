3360cd8fa1d42903e713147c8292f3291016dbf2
SHITOL-CSE-RU <shitol.cse@gmail.com>
2022-04-14 18:39:50 +0600

    Footer Section Done & Solving all Error

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 import "./Footer.css"
+import {FaFacebookF} from "react-icons/fa"
+import {FiInstagram} from "react-icons/fi"
+import {IoLogoTwitter} from "react-icons/io"
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">SHITOL SARKER</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonial">Testimonial</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/shitol.cse"><FaFacebookF /></a>
+        <a href="https://www.instagram.com/shitol.cse"><FiInstagram /></a>
+        <a href="https://www.twitter.com"><IoLogoTwitter /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; SHITOL SARKER. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 export default Footer;
