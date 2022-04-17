ebb86b8ec97fc20e06ca9061953d8984a2bfaf16
SHITOL SARKER <94823753+SHITOL-CSE-RU@users.noreply.github.com>
2022-04-18 02:31:43 +0600

    Update

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -29,7 +29,7 @@ export const Testimonials = () => {
           <div className="client__avatar">
             <img src={AVTR1} alt="Client Avater" />
           </div>
-          <h5 className="client__name">ultrapixelwebde</h5>
+          <h5 className="client__name">"Selinams"</h5>
           <small className="client__review">
           Extremely precise work. Super professionally implemented and the most important thing, 
           people think independently here. thank you I'm very satisfied every time.
@@ -39,7 +39,7 @@ export const Testimonials = () => {
           <div className="client__avatar">
             <img src={AVTR2} alt="Client Avater" />
           </div>
-          <h5 className="client__name">pietrik13</h5>
+          <h5 className="client__name">"pietrik13"</h5>
           <small className="client__review">
           Working with shitol allows me to concentrate on business. He works perfectly on my web sites. 
           Fixes them and improves them. Will keep coming back!
@@ -49,7 +49,7 @@ export const Testimonials = () => {
           <div className="client__avatar">
             <img src={AVTR3} alt="Client Avater" />
           </div>
-          <h5 className="client__name">adamdjbrett</h5>
+          <h5 className="client__name">"adamdjbrett"</h5>
           <small className="client__review">
           It was a great experience working with you! Answered all my questions before we started, 
           showed me outstanding samples of his work, and answered the questions I had after! Amazing to work with!
@@ -59,7 +59,7 @@ export const Testimonials = () => {
           <div className="client__avatar">
             <img src={AVTR4} alt="Client Avater" />
           </div>
-          <h5 className="client__name">luckytuvshee</h5>
+          <h5 className="client__name">"luckytuvshee"</h5>
           <small className="client__review">
           The best experience I have had working with anyone on the site. 
           He is a knowledgeable and thorough developer. Truly an expert in REACT.JS and NODE.JS. Book him today
